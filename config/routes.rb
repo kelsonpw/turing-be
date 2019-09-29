082ba7483737e2d9117f296fca4fec6dd901e747
Kelson Warner <kelson.warner@fundingcircle.com>
2019-09-28 17:08:09 -0700

    Added multiple GET routes for /departments and /categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
- Rails.application.routes.draw do
+Rails.application.routes.draw do
   get "/", to: "application#welcome"
-  scope 'attributes' do
+  scope "attributes" do
     get "/", to: "attribute#get_all_attributes"
     get "/:attribute_id", to: "attribute#get_single_attribute"
     get "/values/:attribute_id", to: "attribute#get_attribute_values"
     get "/inProduct/:product_id", to: "attribute#get_product_attributes"
   end
 
-  scope 'customers' do
+  scope "customers" do
     post "/", to: "customer#update_credit_card"
     post "/login", to: "customer#login"
   end
 
-  scope 'customer' do
+  scope "customer" do
     get "/", to: "customer#get_customer_profile"
     put "/", to: "customer#update_customer_profile"
     put "/address", to: "customer#update_customer_address"
     put "/creditCard", to: "customer#update_credit_card"
   end
 
-  scope 'products' do
+  scope "products" do
     get "/", to: "product#get_all_products"
     get "/:product_id", to: "product#get_product"
     get "/search", to: "product#search_product"
@@ -27,23 +27,24 @@
     get "/inDepartment/:department_id", to: "product#get_products_by_department"
   end
 
-  scope 'departments' do
+  scope "departments" do
     get "/", to: "product#get_all_departments"
     get "/:department_id", to: "product#get_department"
   end
 
-  scope 'categories' do
+  scope "categories" do
     get "/", to: "product#get_all_categories"
     get "/:category_id", to: "product#get_category"
+    get "/inProduct/:product_id", to: "product#get_product_categories"
     get "/inDepartment/:department_id", to: "product#get_department_categories"
   end
 
-  scope 'shipping' do
+  scope "shipping" do
     get "/regions", to: "shipping#get_shipping_regions"
     get "/regions/:shipping_region_id", to: "shipping#get_shipping_type"
   end
 
-  scope 'shoppingcart' do
+  scope "shoppingcart" do
     get "/generateUniqueId", to: "shopping_cart#generate_unique_cart"
     post "/add", to: "shopping_cart#add_item_to_cart"
     get "/:cart_id", to: "shopping_cart#get_cart"
@@ -52,7 +53,7 @@
     delete "/removeProduct/:item_id", to: "shopping_cart#remove_item_from_cart"
   end
 
-  scope 'orders' do
+  scope "orders" do
     post "/", to: "shopping_cart#create_order"
     get "/inCustomer", to: "shopping_cart#get_customer_orders"
     get "/:order_id", to: "shopping_cart#get_order_summary"
@@ -60,9 +61,8 @@
 
   post "/stripe/charge", to: "shopping_cart#process_stripe_payment"
 
-  scope 'task' do
+  scope "task" do
     get "/", to: "tax#get_all_tax"
     get "/:tax_id", to: "tax#get_single_tax"
   end
-
 end
