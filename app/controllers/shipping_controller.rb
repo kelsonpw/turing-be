d75f2489931abd4c5fc82f41304b55e9b1a75dab
Kelson Warner <kelson@null.net>
2019-10-05 17:57:10 -0700

    Shipping Region and Tax GET Requests (#4)
    * Added /shipping/regions GET endpoints
    
    * Added /tax GET endpoints

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -7,13 +7,22 @@
 # to see our recommended endpoints, request body/param, and response object for each of these method
 
 class ShippingController < ApplicationController
+  before_action :authenticate_request!
   # get all shipping regions
   def get_shipping_regions
-    json_response({ message: 'NOT IMPLEMENTED' })
+    shipping_regions = ShippingRegion.all
+    json_response(shipping_regions)
   end
 
   # get all shipping type for a shipping region
   def get_shipping_type
-    json_response({ message: 'NOT IMPLEMENTED' })
+    shipping_region = ShippingRegion.find_by_shipping_region_id(shipping_region_id)
+    json_response(shipping_region)
+  end
+
+  private
+
+  def shipping_region_id
+    params.require(:shipping_region_id)
   end
 end
